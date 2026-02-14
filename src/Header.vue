ab10ffca96190acf54450a0b55c9ffba10ff1c6c
veto <veto@lab.local>
2026-02-14 12:28:36 +0700

    up: src/Header.vue src/index.html

--- src/Header.vue
+++ src/Header.vue
@@ -1,18 +1,18 @@
 <!-- Header.vue -->
 <template>
-  <header class="container-fluid fixed-header">
-      <nav>
-            <ul>
-	            <li><strong>My App</strong></li>
-		          </ul>
-       <ul>
-				        <li><a href="#">Home</a></li>
-					        <li><a href="#features" class="install_app">Install App</a></li>
-						        <li><a class="contrast refresh" href="#signup">Refresh</a></li>
-							      </ul>
-							          </nav>
-								    </header>
-								    </template>
+<header class="container-fluid is-fixed-above-lg">
+  <nav>
+    <ul>
+      <li><strong>My App</strong></li>
+    </ul>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#features" class="install_app">Install App</a></li>
+      <li><a class="contrast refresh" href="#signup">Refresh</a></li>
+    </ul>
+  </nav>
+</header>
+   </template>
 
 
 <style scoped>
